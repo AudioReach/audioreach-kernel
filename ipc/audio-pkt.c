0e36a62767a62c1fd45a594e2941c5bce30df986
ffrancis123 <ffrancis@qti.qualcomm.com>
2025-12-10 16:10:56 +0530

    Update audio-pkt.c
    Signed-off-by: ffrancis123 <ffrancis@qti.qualcomm.com>

--- ipc/audio-pkt.c
+++ ipc/audio-pkt.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
 
-
 #include <linux/platform_device.h>
 #include <linux/of_platform.h>
 #include <linux/refcount.h>
