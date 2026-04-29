0675a6edf890299c326372a8e812d32744e58117
ffrancis123 <ffrancis@qti.qualcomm.com>
2026-04-29 15:15:55 +0530

    Update files_to_copy.sh
    Signed-off-by: ffrancis123 <ffrancis@qti.qualcomm.com>

--- ci/files_to_copy.sh
+++ ci/files_to_copy.sh
@@ -4,5 +4,6 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 cd ..
 
+
 # copy the build artifacts to a temporary directory
 cp  -R build/* /tmp/rootfs/lib/modules/*/updates
