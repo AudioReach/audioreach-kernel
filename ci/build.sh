0e224f5f1379b4114c8c766536cdcb741f7fd2eb
ffrancis <ffrancis@qti.qualcomm.com>
2026-07-02 13:39:44 +0530

    ci files
    Signed-off-by: ffrancis <ffrancis@qti.qualcomm.com>

---
+++ ci/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+set -ex
+PREBUILD_SCRIPT_PATH="${PREBUILD_SCRIPT:-$(dirname "${BASH_SOURCE[0]}")/pre_build.sh}"
+source "$PREBUILD_SCRIPT_PATH"
+
+# load build args from file if environment variable is not set
+if [ -z "${BUILD_ARGS:-}" ]; then
+    BUILD_OPTIONS_FILE="${GITHUB_WORKSPACE}/ci/build_options.txt"
+    BUILD_ARGS="$(sed -E 's/#.*$//' "$BUILD_OPTIONS_FILE" | sed '/^[[:space:]]*$/d' | tr '\n' ' ')"
+fi
+
+
+echo "Running build script..."
+# Build/Compile audioreach-kernel
+source ${GITHUB_WORKSPACE}/install/environment-setup-armv8-2a-qcom-linux
+
+# Prepare Kernel module
+cd $PKG_CONFIG_SYSROOT_DIR/lib/modules/*/build
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules_prepare
+cd -
+make KERNEL_SRC=$PKG_CONFIG_SYSROOT_DIR/lib/modules/*/build/ modules
+mkdir ${GITHUB_WORKSPACE}/build
+cp -r audioreach-driver/*.ko ${GITHUB_WORKSPACE}/build/
+
+
---
+++ ci/files_to_copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+cd ..
+
+
+# copy the build artifacts to a temporary directory
+cp  -R build/* /tmp/rootfs/lib/modules/*/updates
